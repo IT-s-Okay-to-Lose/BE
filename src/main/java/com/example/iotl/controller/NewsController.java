7fc7655dfb297e17cb424de4658cfec3b0ba6bef
Hyewon Lee <hooho931@gmail.com>
2025-06-18 10:40:32 +0900

    [FEAT] swagger 설명 추가

--- src/main/java/com/example/iotl/controller/NewsController.java
+++ src/main/java/com/example/iotl/controller/NewsController.java
@@ -8,10 +8,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "News API", description = "뉴스 관련 API (Naver News OpenAPI 기반)")
 @RestController
 @RequestMapping("/api/v1/news")
 @RequiredArgsConstructor
@@ -19,6 +22,14 @@ public class NewsController {
 
     private final NewsService newsService;
 
+    @Operation(
+            summary = "랜덤 뉴스 3개 조회",
+            description = "네이버 뉴스에서 최근 뉴스 중 랜덤으로 3개를 반환합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "뉴스 3건 조회 성공"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 발생")
+    })
     @GetMapping("/top3")
     public ResponseEntity<List<NewsDto>> getTop3() {
         return ResponseEntity.ok(newsService.getTop3RandomNews().getArticles());
