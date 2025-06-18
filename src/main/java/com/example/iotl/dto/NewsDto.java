11e871924512f400fddfc94e2a85ac9a8b3591f6
Hyewon Lee <hooho931@gmail.com>
2025-06-18 10:40:32 +0900

    [FEAT] swagger 설명 추가

--- src/main/java/com/example/iotl/dto/NewsDto.java
+++ src/main/java/com/example/iotl/dto/NewsDto.java
@@ -5,10 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 // 프론트에 요청 dto
+@Schema(description = "뉴스 정보 DTO")
 @Data
 @AllArgsConstructor
 public class NewsDto {
-    @Schema(description = "아이디", example = "1")
+    @Schema(description = "뉴스 ID", example = "14a5ad9f-4f51-46ad-bd9d-35de35304207")
     private String id;
     @Schema(description = "뉴스 제목", example = "코스피 2,700선 돌파... 외국인 매수세 유입")
     private String title;
