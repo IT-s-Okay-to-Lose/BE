b73ec72defd7aea286d9dfa8205a299e665b892f
Hyewon Lee <hooho931@gmail.com>
2025-06-18 22:13:44 +0900

    [FEAT] 보유 종목 비율 구현

--- src/main/java/com/example/iotl/service/DashboardService.java
+++ src/main/java/com/example/iotl/service/DashboardService.java
@@ -1,9 +1,13 @@
 package com.example.iotl.service;
 
 import com.example.iotl.dto.dashboard.UserInvestmentSummaryDto;
+import com.example.iotl.dto.holding.HoldingRatioDto;
+
+import java.util.List;
 
 
 public interface DashboardService {
     UserInvestmentSummaryDto getInvestmentSummary(Long userId);
+    List<HoldingRatioDto> getHoldingRatio(Long userId);
 }
 
