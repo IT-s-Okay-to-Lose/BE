770bec6a3af10ca865fc10a53ccf17c5291b2cf4
Hyewon Lee <hooho931@gmail.com>
2025-06-19 10:13:33 +0900

    [TEMP] CORS 임시 허용

--- src/main/java/com/example/iotl/controller/NewsController.java
+++ src/main/java/com/example/iotl/controller/NewsController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@CrossOrigin(origins = "*") // 또는 허용할 도메인만 지정
 @Tag(name = "News API", description = "뉴스 관련 API (Naver News OpenAPI 기반)")
 @RestController
 @RequestMapping("/api/v1/news")
