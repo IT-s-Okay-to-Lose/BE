20da0bc09bdffd75e0019f5b503306744abf1268
Hyewon Lee <hooho931@gmail.com>
2025-06-19 11:26:47 +0900

    [FIX] Trade user_id 참조 오류 해결

--- src/main/java/com/example/iotl/repository/HoldingsRepository.java
+++ src/main/java/com/example/iotl/repository/HoldingsRepository.java
@@ -4,7 +4,9 @@ import com.example.iotl.entity.Holdings;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HoldingsRepository extends JpaRepository<Holdings,Long> {
     List<Holdings> findByUser_UserId(Long userId);
+    Optional<Holdings> findByUser_UserIdAndStock_StockCode(Long userId, String stockCode);
 }
