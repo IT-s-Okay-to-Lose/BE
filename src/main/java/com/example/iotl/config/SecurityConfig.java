066c6dc69b6a554b366318c343e804bd138ffc51
Hyewon Lee <hooho931@gmail.com>
2025-06-21 18:46:44 +0900

    [TEMP] security 임시 주석 처리

--- src/main/java/com/example/iotl/config/SecurityConfig.java
+++ src/main/java/com/example/iotl/config/SecurityConfig.java
@@ -1,107 +1,107 @@
-package com.example.iotl.config;
-
-import com.example.iotl.filter.JWTFilter;
-import com.example.iotl.handler.CustomSuccessHandler;
-import com.example.iotl.jwt.JWTUtil;
-import com.example.iotl.service.CustomOAuth2UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Collections;
-import java.util.List;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final CustomSuccessHandler customSuccessHandler;
-    private final JWTUtil jwtUtil;
-
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
-        this.customOAuth2UserService = customOAuth2UserService;
-        this.customSuccessHandler = customSuccessHandler;
-        this.jwtUtil = jwtUtil;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, CustomSuccessHandler customSuccessHandler) throws Exception {
-
-        //csrf disable
-        http
-                .csrf((auth) -> auth.disable());
-
-        //From 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
-
-        //HTTP Basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
-
-        //JWTFilter 추가
-        http
-                .addFilterAfter(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
-
-        //oauth2
-        http
-                .oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService))
-                        .successHandler(customSuccessHandler));
-
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers(
-                                "/", "/login", "/oauth2/**", "/login/oauth2/**", "/reissue",
-                                "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
-                                "/swagger-resources/**", "/webjars/**"
-                        ).permitAll()
-                        .anyRequest().authenticated());
-
-        //세션 설정 : STATELESS
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-                        CorsConfiguration configuration = new CorsConfiguration();
-
-                        configuration.setAllowedOrigins(List.of(
-                                "https://iotl-fe.vercel.app", // 실제 프론트엔드
-                                "https://iotl.store"          // Swagger UI 주소
-                        ));
-
-//                        configuration.setAllowedOrigins(Collections.singletonList("https://iotl-fe.vercel.app"));
-//                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); //배포시에는 프론트엔드 도메인으로 대체
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
-
-//                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-//                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-                        configuration.setExposedHeaders(List.of("Set-Cookie", "Authorization"));
-
-                        return configuration;
-                    }
-                }));
-
-
-        return http.build();
-    }
-}
+//package com.example.iotl.config;
+//
+//import com.example.iotl.filter.JWTFilter;
+//import com.example.iotl.handler.CustomSuccessHandler;
+//import com.example.iotl.jwt.JWTUtil;
+//import com.example.iotl.service.CustomOAuth2UserService;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    private final CustomOAuth2UserService customOAuth2UserService;
+//    private final CustomSuccessHandler customSuccessHandler;
+//    private final JWTUtil jwtUtil;
+//
+//    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
+//        this.customOAuth2UserService = customOAuth2UserService;
+//        this.customSuccessHandler = customSuccessHandler;
+//        this.jwtUtil = jwtUtil;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http, CustomSuccessHandler customSuccessHandler) throws Exception {
+//
+//        //csrf disable
+//        http
+//                .csrf((auth) -> auth.disable());
+//
+//        //From 로그인 방식 disable
+//        http
+//                .formLogin((auth) -> auth.disable());
+//
+//        //HTTP Basic 인증 방식 disable
+//        http
+//                .httpBasic((auth) -> auth.disable());
+//
+//        //JWTFilter 추가
+//        http
+//                .addFilterAfter(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+//
+//        //oauth2
+//        http
+//                .oauth2Login((oauth2) -> oauth2
+//                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+//                                .userService(customOAuth2UserService))
+//                        .successHandler(customSuccessHandler));
+//
+//        //경로별 인가 작업
+//        http
+//                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers(
+//                                "/", "/login", "/oauth2/**", "/login/oauth2/**", "/reissue",
+//                                "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
+//                                "/swagger-resources/**", "/webjars/**"
+//                        ).permitAll()
+//                        .anyRequest().authenticated());
+//
+//        //세션 설정 : STATELESS
+//        http
+//                .sessionManagement((session) -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//        http
+//                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+//
+//                    @Override
+//                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//
+//                        CorsConfiguration configuration = new CorsConfiguration();
+//
+//                        configuration.setAllowedOrigins(List.of(
+//                                "https://iotl-fe.vercel.app", // 실제 프론트엔드
+//                                "https://iotl.store"          // Swagger UI 주소
+//                        ));
+//
+////                        configuration.setAllowedOrigins(Collections.singletonList("https://iotl-fe.vercel.app"));
+////                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); //배포시에는 프론트엔드 도메인으로 대체
+//                        configuration.setAllowedMethods(Collections.singletonList("*"));
+//                        configuration.setAllowCredentials(true);
+//                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+//                        configuration.setMaxAge(3600L);
+//
+////                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+////                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//                        configuration.setExposedHeaders(List.of("Set-Cookie", "Authorization"));
+//
+//                        return configuration;
+//                    }
+//                }));
+//
+//
+//        return http.build();
+//    }
+//}
