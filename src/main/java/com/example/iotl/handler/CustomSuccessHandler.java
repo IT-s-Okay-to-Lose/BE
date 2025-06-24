148b942b79419f9d696af5df7389fab5a631a9ea
hojinLeo <201902936@hufs.ac.kr>
2025-06-24 11:49:34 +0900

    [TEST] 로컬 url -> https로 수정

--- src/main/java/com/example/iotl/handler/CustomSuccessHandler.java
+++ src/main/java/com/example/iotl/handler/CustomSuccessHandler.java
@@ -139,6 +139,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         log.info("Redirecting to front - main Page After Successful Login");
         // ✅ 리다이렉트로 프론트 진입
 //        response.sendRedirect("https://iotl-fe.vercel.app/");
-        response.sendRedirect("http://localhost:5137");
+        response.sendRedirect("https://localhost:5137/");
     }
 }
